83350bb1486e90d3300276f3ab66dab7a8a49635
Seu Nome <caiopolis13@gmail.com>
2025-02-19 17:58:25 -0300

    exercicio 2

---
+++ JSON/exercicio_JSON/exercicio2.js
@@ -0,0 +1,24 @@
+
+// exercicio 2-A
+const produt0 = {
+    id: 123456,
+    nome: "celular",
+    preco: 1000.00
+}
+
+const stringJASON = JSON.stringify(produt0)
+console.log(stringJASON)
+
+// exercicio 2-B
+
+const objectAnimais = require("./animais.json")
+objectAnimais.animais.push({
+    id: 14,
+    nome: "Gorila",
+    tipo: "mamifero",
+    habitat: "florestas",
+}
+)
+objectAnimais.animais[2].habitat = "Floresta Amazonica"
+objectAnimais.animais.splice(1,1)
+console.log(objectAnimais.animais)
